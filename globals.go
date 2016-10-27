b606e999128a27b53f44279734dddd10da15ac4e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-27 09:36:45 -0400

    init afero

--- globals.go
+++ globals.go
@@ -1,7 +1,11 @@
 package dfm
 
+import "github.com/spf13/afero"
+
 // CONFIG holds the global config struct
 var CONFIG Config
 
 // DRYRUN is used to globally set whether this is a dry run
 var DRYRUN = false
+
+var FS = afero.NewOsFs()
